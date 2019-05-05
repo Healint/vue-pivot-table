df9f97a0b624c7dbae36118309ce6493c3bc6356
Lim Jiechao <jiechao@healint.com>
2019-05-05 23:33:45 +0800

    (1) removed last `isDataLoading` prop; (2) new build

--- src/Pivot.vue
+++ src/Pivot.vue
@@ -63,7 +63,7 @@
       <!-- Table -->
       <div class="col table-responsive">
         <!-- NOTE: Customization -->
-        <pivot-table :data="data" :row-fields="internal.rowFields" :col-fields="internal.colFields" :reducer="reducer" :no-data-warning-text="noDataWarningText" :is-data-loading="isDataLoading" :values-to-display="valuesToDisplay" :aggregation-logic="aggregationLogic" :aggregation-field="aggregationField">
+        <pivot-table :data="data" :row-fields="internal.rowFields" :col-fields="internal.colFields" :reducer="reducer" :no-data-warning-text="noDataWarningText" :values-to-display="valuesToDisplay" :aggregation-logic="aggregationLogic" :aggregation-field="aggregationField">
           <!-- pass down scoped slots -->
           <template v-for="(slot, slotName) in $scopedSlots" :slot="slotName" slot-scope="{ value }">
             <slot :name="slotName" v-bind="{ value }"></slot>
