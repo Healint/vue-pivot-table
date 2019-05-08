ee3e4e08efc3e5b339dad9d27054435864e97e2a
Lim Jiechao <jiechao@healint.com>
2019-05-08 20:54:28 +0800

    allow use as by `Vue.use(…)`, use as module, and auto-install when vue is found

--- src/browser.js
+++ src/browser.js
@@ -1,7 +1,30 @@
-import Pivot from './Pivot'
-import PivotTable from './PivotTable'
+// Import vue components
+import Pivot from './Pivot';
+import PivotTable from './PivotTable';
 
-export function install(Vue, options) {
-  Vue.component('pivot', Pivot)
-  Vue.component('pivot-table', PivotTable)
+// Declare install function executed by Vue.use()
+export default function install (Vue, options) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component('pivot', Pivot);
+  Vue.component('pivot-table', PivotTable);
 }
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+// Create module definition for Vue.use()
+const plugin = { install };
+
+// To allow use as module (npm/webpack/etc.) export component
+export const DynamicPivotTable = Pivot;
+export const StaticPivotTable = PivotTable;
+
