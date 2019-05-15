0c927ce4cfcc5d1c589b3787ff0d40fc24129304
Lim Jiechao <jiechao@healint.com>
2019-05-15 16:17:32 +0800

    filter for categorical field values

--- src/Pivot.vue
+++ src/Pivot.vue
@@ -18,6 +18,7 @@
           {{ availableFieldsLabelText }}
         </div>
         <draggable
+          :disabled="modalIsOpen"
           v-model="internal.fields"
           class="d-flex flex-row drag-area flex-wrap"
           :class="dragAreaClass"
@@ -26,8 +27,89 @@
           @end="end">
           <div
             v-for="field in internal.fields"
-            :key="field.key">
-            <div class="btn btn-draggable btn-secondary">
+            :key="field.label">
+            <details
+              v-if="fieldFilters.hasOwnProperty(field.label)"
+              :id="`details-${field.label.toLowerCase().split(' ').join('-')}`"
+              @toggle="toggleModal"
+              class="btn btn-draggable btn-secondary">
+              <summary>
+                <svg
+                  aria-hidden="true"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 270 512"
+                  class="svg-inline--fa fa-grip-vertical fa-w-10">
+                  <path
+                    fill="currentColor"
+                    d="M64 208c26.5 0 48 21.5 48 48s-21.5 48-48 48-48-21.5-48-48 21.5-48 48-48zM16 104c0 26.5 21.5 48 48 48s48-21.5 48-48-21.5-48-48-48-48 21.5-48 48zm0 304c0 26.5 21.5 48 48 48s48-21.5 48-48-21.5-48-48-48-48 21.5-48 48z M204 208c26.5 0 48 21.5 48 48s-21.5 48 -48 48 -48 -21.5 -48 -48 21.5 -48 48 -48zM156 104c0 26.5 21.5 48 48 48s48 -21.5 48 -48 -21.5 -48 -48 -48 -48 21.5 -48 48zm0 304c0 26.5 21.5 48 48 48s48 -21.5 48 -48 -21.5 -48 -48 -48 -48 21.5 -48 48z"
+                    class=""/>
+                </svg>
+                {{ field.label }}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="filter"
+                  class="small svg-inline--fa fa-filter fa-w-10"
+                  role="img"
+                  viewBox="0 0 512 512"><path
+                    fill="currentColor"
+                    d="M487.976 0H24.028C2.71 0-8.047 25.866 7.058 40.971L192 225.941V432c0 7.831 3.821 15.17 10.237 19.662l80 55.98C298.02 518.69 320 507.493 320 487.98V225.941l184.947-184.97C520.021 25.896 509.338 0 487.976 0z"/>
+                </svg>
+              </summary>
+              <div
+                class="filter-modal modal-content"
+                style="width: fit-content; min-width: 30vmin;">
+                <div class="modal-header">
+                  <h5 class="modal-title">
+                    {{ field.label }}
+                  </h5>
+                </div>
+                <div class="modal-body">
+                  <div class="form-group">
+                    <div
+                      v-for="(value, key) in fieldFilters[field.label]"
+                      :key="`${field.label}-${key}`"
+                      class="form-check">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        :value="key"
+                        :id="key"
+                        v-model="fieldFilters[field.label][key]"
+                        checked>
+                      <label
+                        class="form-check-label"
+                        :for="key">
+                        {{ key }}
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="modal-footer"
+                  style="justify-content: space-between;">
+                  <button
+                    type="button"
+                    @click="dismissModal(`details-${field.label.toLowerCase().split(' ').join('-')}`)"
+                    class="btn btn-secondary"
+                    data-dismiss="modal">
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    @click="applyFieldFilters(`details-${field.label.toLowerCase().split(' ').join('-')}`)"
+                    class="btn btn-primary">
+                    Apply
+                  </button>
+                </div>
+              </div>
+            </details>
+            <div
+              v-else
+              class="btn btn-draggable btn-secondary">
               <svg
                 aria-hidden="true"
                 role="img"
@@ -68,16 +150,98 @@
           {{ colsLabelText }}
         </div>
         <draggable
+          :disabled="modalIsOpen"
           v-model="internal.colFields"
+          class="d-flex flex-row drag-area flex-wrap"
+          :class="dragAreaClass"
           group="fields"
           @start="start"
-          @end="end"
-          class="d-flex flex-row drag-area border-primary"
-          :class="dragAreaClass">
+          @end="end">
           <div
             v-for="field in internal.colFields"
-            :key="field.key">
-            <div class="btn btn-draggable btn-primary">
+            :key="field.label">
+            <details
+              v-if="fieldFilters.hasOwnProperty(field.label)"
+              :id="`details-${field.label.toLowerCase().split(' ').join('-')}`"
+              @toggle="toggleModal"
+              class="btn btn-draggable btn-primary">
+              <summary>
+                <svg
+                  aria-hidden="true"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 270 512"
+                  class="svg-inline--fa fa-grip-vertical fa-w-10">
+                  <path
+                    fill="currentColor"
+                    d="M64 208c26.5 0 48 21.5 48 48s-21.5 48-48 48-48-21.5-48-48 21.5-48 48-48zM16 104c0 26.5 21.5 48 48 48s48-21.5 48-48-21.5-48-48-48-48 21.5-48 48zm0 304c0 26.5 21.5 48 48 48s48-21.5 48-48-21.5-48-48-48-48 21.5-48 48z M204 208c26.5 0 48 21.5 48 48s-21.5 48 -48 48 -48 -21.5 -48 -48 21.5 -48 48 -48zM156 104c0 26.5 21.5 48 48 48s48 -21.5 48 -48 -21.5 -48 -48 -48 -48 21.5 -48 48zm0 304c0 26.5 21.5 48 48 48s48 -21.5 48 -48 -21.5 -48 -48 -48 -48 21.5 -48 48z"
+                    class=""/>
+                </svg>
+                {{ field.label }}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="filter"
+                  class="small svg-inline--fa fa-filter fa-w-10"
+                  role="img"
+                  viewBox="0 0 512 512"><path
+                    fill="currentColor"
+                    d="M487.976 0H24.028C2.71 0-8.047 25.866 7.058 40.971L192 225.941V432c0 7.831 3.821 15.17 10.237 19.662l80 55.98C298.02 518.69 320 507.493 320 487.98V225.941l184.947-184.97C520.021 25.896 509.338 0 487.976 0z"/>
+                </svg>
+              </summary>
+              <div
+                class="filter-modal modal-content"
+                style="width: fit-content; min-width: 30vmin;">
+                <div class="modal-header">
+                  <h5 class="modal-title">
+                    {{ field.label }}
+                  </h5>
+                </div>
+                <div class="modal-body">
+                  <div class="form-group">
+                    <div
+                      v-for="(value, key) in fieldFilters[field.label]"
+                      :key="`${field.label}-${key}`"
+                      class="form-check">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        :value="key"
+                        :id="key"
+                        v-model="fieldFilters[field.label][key]"
+                        checked>
+                      <label
+                        class="form-check-label"
+                        :for="key">
+                        {{ key }}
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="modal-footer"
+                  style="justify-content: space-between;">
+                  <button
+                    type="button"
+                    @click="dismissModal(`details-${field.label.toLowerCase().split(' ').join('-')}`)"
+                    class="btn btn-secondary"
+                    data-dismiss="modal">
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    @click="applyFieldFilters(`details-${field.label.toLowerCase().split(' ').join('-')}`)"
+                    class="btn btn-primary">
+                    Apply
+                  </button>
+                </div>
+              </div>
+            </details>
+            <div
+              v-else
+              class="btn btn-draggable btn-secondary">
               <svg
                 aria-hidden="true"
                 role="img"
@@ -105,16 +269,98 @@
           {{ rowsLabelText }}
         </div>
         <draggable
+          :disabled="modalIsOpen"
           v-model="internal.rowFields"
+          class="d-flex flex-row drag-area flex-wrap"
+          :class="dragAreaClass"
           group="fields"
           @start="start"
-          @end="end"
-          class="d-flex flex-column align-items-start drag-area border-primary"
-          :class="dragAreaClass">
+          @end="end">
           <div
             v-for="field in internal.rowFields"
-            :key="field.key">
-            <div class="btn btn-draggable btn-primary">
+            :key="field.label">
+            <details
+              v-if="fieldFilters.hasOwnProperty(field.label)"
+              :id="`details-${field.label.toLowerCase().split(' ').join('-')}`"
+              @toggle="toggleModal"
+              class="btn btn-draggable btn-primary">
+              <summary>
+                <svg
+                  aria-hidden="true"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 270 512"
+                  class="svg-inline--fa fa-grip-vertical fa-w-10">
+                  <path
+                    fill="currentColor"
+                    d="M64 208c26.5 0 48 21.5 48 48s-21.5 48-48 48-48-21.5-48-48 21.5-48 48-48zM16 104c0 26.5 21.5 48 48 48s48-21.5 48-48-21.5-48-48-48-48 21.5-48 48zm0 304c0 26.5 21.5 48 48 48s48-21.5 48-48-21.5-48-48-48-48 21.5-48 48z M204 208c26.5 0 48 21.5 48 48s-21.5 48 -48 48 -48 -21.5 -48 -48 21.5 -48 48 -48zM156 104c0 26.5 21.5 48 48 48s48 -21.5 48 -48 -21.5 -48 -48 -48 -48 21.5 -48 48zm0 304c0 26.5 21.5 48 48 48s48 -21.5 48 -48 -21.5 -48 -48 -48 -48 21.5 -48 48z"
+                    class=""/>
+                </svg>
+                {{ field.label }}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="filter"
+                  class="small svg-inline--fa fa-filter fa-w-10"
+                  role="img"
+                  viewBox="0 0 512 512"><path
+                    fill="currentColor"
+                    d="M487.976 0H24.028C2.71 0-8.047 25.866 7.058 40.971L192 225.941V432c0 7.831 3.821 15.17 10.237 19.662l80 55.98C298.02 518.69 320 507.493 320 487.98V225.941l184.947-184.97C520.021 25.896 509.338 0 487.976 0z"/>
+                </svg>
+              </summary>
+              <div
+                class="filter-modal modal-content"
+                style="width: fit-content; min-width: 30vmin;">
+                <div class="modal-header">
+                  <h5 class="modal-title">
+                    {{ field.label }}
+                  </h5>
+                </div>
+                <div class="modal-body">
+                  <div class="form-group">
+                    <div
+                      v-for="(value, key) in fieldFilters[field.label]"
+                      :key="`${field.label}-${key}`"
+                      class="form-check">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        :value="key"
+                        :id="key"
+                        v-model="fieldFilters[field.label][key]"
+                        checked>
+                      <label
+                        class="form-check-label"
+                        :for="key">
+                        {{ key }}
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="modal-footer"
+                  style="justify-content: space-between;">
+                  <button
+                    type="button"
+                    @click="dismissModal(`details-${field.label.toLowerCase().split(' ').join('-')}`)"
+                    class="btn btn-secondary"
+                    data-dismiss="modal">
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    @click="applyFieldFilters(`details-${field.label.toLowerCase().split(' ').join('-')}`)"
+                    class="btn btn-primary">
+                    Apply
+                  </button>
+                </div>
+              </div>
+            </details>
+            <div
+              v-else
+              class="btn btn-draggable btn-secondary">
               <svg
                 aria-hidden="true"
                 role="img"
@@ -135,7 +381,7 @@
       <!-- Table -->
       <div class="col table-responsive">
         <pivot-table
-          :data="data"
+          :data="filteredData"
           :row-fields="internal.rowFields"
           :col-fields="internal.colFields"
           :reducer="reducer"
@@ -245,12 +491,39 @@ export default {
         colFields: this.colFields
       },
       dragging: false,
-      showSettings: true
+      showSettings: true,
+      modalIsOpen: false,
+      fieldFilters: {},
+      filteredData: []
     }
   },
   computed: {
     dragAreaClass: function () {
       return this.dragging ? 'drag-area-highlight' : null
+    },
+    fieldsToFilter: function () {
+      return (
+        Object.entries(this.fieldFilters)
+          .map(
+            ([key, values]) => {
+              return [
+                key,
+                Object.entries(values)
+                  .filter(([key, boolean]) => !boolean)
+                  .map(([key, boolean]) => key)
+              ]
+            }
+          )
+          .filter(
+            ([key, values]) => !!values.length
+          )
+          .reduce(
+            (fields, [key, values]) => {
+              return { ...fields, [key]: values }
+            },
+            {}
+          )
+      )
     }
   },
   methods: {
@@ -262,43 +535,123 @@ export default {
     },
     end: function () {
       this.dragging = false
+    },
+    toggleModal () {
+      this.modalIsOpen = !this.modalIsOpen
+    },
+    dismissModal (detailsId) {
+      document.getElementById(detailsId).open = false
+    },
+    applyFieldFilters (detailsId) {
+      this.filteredData = [...this.filterData()]
+      document.getElementById(detailsId).open = false
+    },
+    constructFieldFilters: function () {
+      if (!this.data.length) { return null }
+      return (
+        Object.keys(this.data[0])
+          // Exclude fields where all values are numbers
+          .filter(
+            field => this.data.some(entry => typeof entry[field] === 'string')
+          )
+          // Map each field to their possible unique values
+          .map(
+            field => {
+              return {
+                [field]: (
+                  // Derive sets of possible values for each fields and sort them
+                  Array.from(new Set(this.data.map(entry => entry[field]))).sort()
+                    // Construct data property for v-model
+                    .map(
+                      value => {
+                        return { [value]: true }
+                      }
+                    )
+                    // Convert array into object
+                    .reduce(
+                      (values, value) => {
+                        return { ...values, ...value }
+                      },
+                      {}
+                    )
+                )
+              }
+            }
+          )
+          // Convert array into object
+          .reduce(
+            (fields, field) => {
+              return { ...fields, ...field }
+            },
+            {}
+          )
+      )
+    },
+    filterData: function () {
+      let entries = Object.entries(this.fieldsToFilter)
+      return (
+        this.data.filter(
+          entry => {
+            return entries.every(
+              ([key, values]) => !values.includes(entry[key])
+            )
+          }
+        )
+      )
     }
   },
   created: function () {
     this.showSettings = this.defaultShowSettings
   },
-  // NOTE: START—Development Only. Do not commit
   watch: {
-    fields: {
-      handler (newValue) {
-        if (newValue.length) {
-          this.internal.fields = this.fields
-        }
-      },
-      immediate: true
+    data: function () {
+      this.fieldFilters = this.constructFieldFilters()
+      this.filteredData = [...this.data]
     },
-    rowFields: {
-      handler (newValue) {
-        if (newValue.length) {
-          this.internal.rowFields = this.rowFields
-        }
-      },
-      immediate: true
-    },
-    colFields: {
-      handler (newValue) {
-        if (newValue.length) {
-          this.internal.colFields = this.colFields
-        }
-      },
-      immediate: true
-    }
   }
-  // NOTE: END—Development Only. Do not commit
 }
 </script>
 
 <style lang="scss" scoped>
+/* Filter Modal */
+summary:focus {
+  outline: none !important;
+}
+details[open] > summary::before {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: block;
+  cursor: default;
+  content: " ";
+  z-index: 99;
+  background: rgba(27,31,35,0.5);
+}
+details > .filter-modal {
+  cursor: default !important;
+  color: initial;
+  left: 50vw;
+  top: 50vh;
+  transform: translate(-50%, -50%);
+  max-height: 80vh;
+  max-width: 90vw;
+  position: fixed;
+  /*width: 50vw;*/
+  z-index: 999;
+  animation-name: fadein;
+  animation-duration: 300ms;
+  animation-timing-function: ease-in-out;
+}
+@keyframes fadein {
+  from { opacity: 0; }
+  to   { opacity: 1; }
+}
+details summary::-webkit-details-marker {
+  display:none;
+}
+
 /* Left column width */
 .left-col {
   min-width: 200px;
