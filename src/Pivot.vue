23100308bd3b0d423ebc97c706c0566867437c11
Lim Jiechao <jiechao@healint.com>
2019-05-16 20:13:40 +0800

    moved filter list icon upwards

--- src/Pivot.vue
+++ src/Pivot.vue
@@ -75,7 +75,7 @@
                   width="24"
                   height="24"
                   role="img"
-                  style="margin: -6px -4px 0 -4px; fill: white;"
+                  style="margin: -4px -4px 0 -4px; fill: white;"
                   viewBox="0 0 24 24">
                   <path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"/>
                   <path
@@ -239,7 +239,7 @@
                   width="24"
                   height="24"
                   role="img"
-                  style="margin: -6px -4px 0 -4px; fill: white;"
+                  style="margin: -4px -4px 0 -4px; fill: white;"
                   viewBox="0 0 24 24">
                   <path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"/>
                   <path
@@ -390,7 +390,7 @@
                   width="24"
                   height="24"
                   role="img"
-                  style="margin: -6px -4px 0 -4px; fill: white;"
+                  style="margin: -4px -4px 0 -4px; fill: white;"
                   viewBox="0 0 24 24">
                   <path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"/>
                   <path
